12eb7d426068aac15db9d03ffc58097f0660f7f5
Ahamed-ammar <ahamedammar25@gmail.com>
2025-11-05 11:22:36 +0530

    All are merged Successfully but user sigup verification is here

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,12 @@
 import { getDBConnection } from '../db/db.js';
+import bcrypt from 'bcryptjs';
 import validator from 'validator';
 export async function registerUser(req, res) {
-    console.log('req.body: ', req.body)
-    console.log('Request Type:- ', typeof req.body)   
-    console.log('Request Keys:- ', Object.keys(req.body))
-    console.log('Request Values:- ', Object.values(req.body))
-    console.log('Request Entries:- ', Object.entries(req.body))
+    // console.log('req.body: ', req.body)
+    // console.log('Request Type:- ', typeof req.body)   
+    // console.log('Request Keys:- ', Object.keys(req.body))
+    // console.log('Request Values:- ', Object.values(req.body))
+    // console.log('Request Entries:- ', Object.entries(req.body))
     let { name, email, username, password } = req.body
 
     if(!name || !email || !username || !password) {
@@ -15,6 +16,8 @@ export async function registerUser(req, res) {
     name = name.trim()
     email = email.trim()
     username = username.trim()
+
+    password = password.trim()
     
     if(!/^[a-zA-Z0-9_-]{1,20}$/.test(username)) {
         return res.status(400).json({ error: 'Username must be 1–20 characters, using letters, numbers, _ or -.' })
@@ -24,15 +27,16 @@ export async function registerUser(req, res) {
         return res.status(400).json({ error: 'Invalid email address.' })
     }
     try {
-        const db = await getDBConnection();
-        const existUser = await db.get('SELECT id FROM users WHERE username = ? OR email = ? ', [username, email]);
-        if(existUser) {
+        const db = await getDBConnection()
+        const existingUser = await db.get('SELECT id FROM users WHERE username = ? OR email = ?', [username, email])
+        if(existingUser) {
             return res.status(400).json({ error: 'Username or email already in use.' })
         }
-        await db.run('INSERT INTO users (name, email, username, password) VALUES (?, ?, ?, ?)', [name, email, username, password]);
-        await db.close();
-        console.log(req.body);
-        return  res.status(201).json({ message: 'User registered successfully' });
+        const hashedPassword = await bcrypt.hash(password, 10)
+        db.run('INSERT INTO users (name, email, username, password) VALUES (?, ?, ?, ?)', 
+            [name, email, username, hashedPassword])
+        await db.close()
+        return res.status(201).json({ message: 'User registered successfully' })
     }
     catch (err) {
         console.error('Database error:', err);
